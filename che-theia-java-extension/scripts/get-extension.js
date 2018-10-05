fbe0bb0f236dfcffd037656e1a292c9588a8683c
Valeriy Svydenko <vsvydenk@redhat.com>
2018-10-05 17:56:44 +0300

    Downloads che-ls-jdt bundle
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

---
+++ che-theia-java-extension/scripts/get-extension.js
@@ -0,0 +1,45 @@
+/********************************************************************************
+ * Copyright (C) 2018 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+const fs = require('fs');
+const process = require('child_process');
+const path = require('path');
+
+const packagePath = path.join(__dirname, '..');
+const version = '0.0.1-SNAPSHOT';
+const archiveExtensionCoreName = `jdt.ls.extension.core-${version}.jar`;
+const archiveExtensionAPIName = `jdt.ls.extension.api-${version}.jar`;
+const downloadDir = 'download';
+const downloadPath = path.join(packagePath, downloadDir);
+const archiveExtensionCorePath = path.join(downloadPath, archiveExtensionCoreName);
+const archiveExtensionAPIPath = path.join(downloadPath, archiveExtensionAPIName);
+
+function getDevServer() {
+    return new Promise((resolve, reject) => {
+        const command = 'mvn dependency:copy ';
+        console.log('executing ' + command);
+        process.exec(command, { cwd: __dirname }, () => {
+            if (fs.existsSync(archiveExtensionCorePath) && fs.existsSync(archiveExtensionAPIPath)) {
+                resolve(archiveExtensionCorePath);
+                resolve(archiveExtensionAPIPath);
+            } else {
+                reject(new Error('Archive file not found.'));
+            }
+        });
+    });
+}
+
+getDevServer();
---
+++ che-theia-java-extension/src/node/java-contribution.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2012-2018 Red Hat, Inc.
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which is available at http://www.eclipse.org/legal/epl-2.0.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+import { injectable } from 'inversify';
+import { JavaExtensionContribution } from '@theia/java/lib/node/java-extension-model';
+import * as path from 'path';
+
+@injectable()
+export class JavaExtension implements JavaExtensionContribution {
+    getExtensionBundles(): string[] {
+        const version = '0.0.1-SNAPSHOT';
+        const downloadDirPath = path.join(__dirname, '/../../download');
+
+        const archiveCoreName = `jdt.ls.extension.core-${version}.jar`;
+        const archiveCorePath = path.join(downloadDirPath, archiveCoreName);
+        
+        const archiveAPIName = `jdt.ls.extension.api-${version}.jar`;
+        const archiveAPIPath = path.join(downloadDirPath, archiveAPIName);
+        
+        return [archiveAPIPath, archiveCorePath];
+    }
+}
+
